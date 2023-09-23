9c6330e31e0cc0c90b35e138615a54490adbba83
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-23 13:36:36 +0530

    static/index.html file served

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{web, App, HttpServer, Result};
+use actix_files::{NamedFile};
 
-#[get("/hello")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("hello world")
-}
-
-struct AppState {
-    app_name: String,
-}
-
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on port 8080");
     HttpServer::new(|| {
-        App::new().app_data(web::Data::new(AppState {
-            app_name: String::from("Actix Web"),
-        }))
-            .service(index)
-            .service(hello)
+        App::new().route("/", web::get().to(index))
     })
     .bind("localhost:8080")?
     .run()
