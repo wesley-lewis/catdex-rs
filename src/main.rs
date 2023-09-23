ae6729a6ee82a1800dc7e0f5c4e173d9ca6054e7
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-23 13:30:01 +0530

    server working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
 
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("hello world")
+}
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on port 8080");
     HttpServer::new(|| {
-        App::new().route("/hello", web::get().to(hello))
+        App::new().app_data(web::Data::new(AppState {
+            app_name: String::from("Actix Web"),
+        }))
+            .service(index)
+            .service(hello)
     })
     .bind("localhost:8080")?
     .run()
