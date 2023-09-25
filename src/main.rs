f7c18ce95ab3a4c06ae771a8fab8d0a437414c69
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-25 20:40:43 +0530

    created dbpool and connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,68 @@
-use actix_web::{web, App, HttpServer, Result};
-use actix_files::{NamedFile};
+use actix_files::{Files, NamedFile};
+use actix_web::web::DbPool;
+use actix_web::{web, App, Error, HttpResponse, HttpServer, Result};
+use std::env;
 
-async fn index() -> Result<NamedFile> {
-    Ok(NamedFile::open("./static/index.html")?)
+use handlebars::Handlebars;
+
+use self::schema::cats::dsl::cats;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+mod models;
+mod schema;
+use self::models::*;
+
+// TODO: solve error regarding following struct
+struct IndexTemplateData {
+    project_name: String,
+    cats: Vec<Cat>,
+}
+
+async fn index(
+    hb: web::Data<Handlebars<'_>>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let connection = pool.get().expect("Can't get db connection from pool");
+
+    let cats_data = web::block(move || cats.limit(100).load::<Cat>(&connection))
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+    let data = IndexTemplateData {
+        project_name: "CatDex".to_string(),
+        cats: cats_data,
+    };
+
+    let body = hb.render("index", &data).unwrap();
+
+    Ok(HttpResponse::Ok().body(body))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+
+    // Setting up the database connection pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(&database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create DB connection pool");
+
+    let handlebars_ref = web::Data::new(handlebars);
+
     println!("Listening on port 8080");
-    HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
+    HttpServer::new(move || {
+        App::new()
+            .app_data(handlebars_ref.clone())
+            .app_data(pool.clone())
+            .service(Files::new("/static", "static").show_files_listing())
+            .route("/", web::get().to(index))
     })
     .bind("localhost:8080")?
     .run()
